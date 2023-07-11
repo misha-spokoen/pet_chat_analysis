e2361cc351c6cfb4a06ad6bd1ebe8898bfa1c721
Михаил <mikhail@Mihails-MacBook-Pro.local>
2023-07-11 23:27:24 +0300

    rewriting RegExp and adding comments

--- chat_analysis.py
+++ chat_analysis.py
@@ -4,12 +4,12 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Загружаем файл JSON
-with open('/Users/mikhail/Desktop/Code/work_with_chat/result.json', 'r') as f:
+# Открываем наш файл JSON
+with open('/Users/mikhail/Desktop/Code/work_with_chat_final/result_full.json', 'r') as f:
     data = json.load(f)
 
 # Описываем в RegExp слово, которое будем искать
-pattern = re.compile(r'(П|п)ив(о|а|ы)', re.IGNORECASE)
+pattern = re.compile(r'\s[Пп]ив[нкчоаы]', re.IGNORECASE)
 
 # Создаем переменные для временных периодов
 morning_period = list(range(4, 12))  # с 4:00 до 12:00
@@ -18,34 +18,39 @@ evening_period = range(17, 24)  # с 17:00 до 00:00
 night_period = list(range(0, 5))  # с 00:00 до 5:00
 
 # Ищем сообщения, которые содержат искомое слово и разделяем их по времени суток
-morning_messages=[]
-day_messages=[]
-evening_messages=[]
-night_messages=[]
+morning_messages = []
+day_messages = []
+evening_messages = []
+night_messages = []
 
 for message in data['messages']:
     if 'text' in message:
+        # Проверяем не принадлежит ли наше сообщение к классу "список"
         if isinstance(message['text'], list):
-            # Здесь мы собираем список строк в единую строку
-            text=' '.join(str(x) for x in message['text'])
+            # Если сообщение список - собираем в строку
+            text = ' '.join(str(x) for x in message['text'])
         else:
-            text=message['text']
-        match=pattern.search(text)
+            # Если нет - присваеваем как есть
+            text = message['text']
+        # Сравниваем результат с паттерном и присваеваем переменной match в случае совпадения
+        match = pattern.search(text)
+        # Если нашлось совпадение - определяем в какой промежуток времени написано сообщение
         if match:
             # Выделяем часы из времени сообщения
-            date_string=message['date']
-            date=datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
-            hour=date.hour
+            date_string = message['date']
+            date = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
+            hour = date.hour
 
-            # Разделяем сообщения по времени суток
+            # Если сообщение принадлежит к временному промежутку - добавляем его в список
+            # Добавляем целиком, что бы можно было вывести сообщения при необходимости
             if hour in morning_period:
-                morning_messages.append(message)
+                morning_messages.append(message['text'])
             elif hour in day_period:
-                day_messages.append(message)
+                day_messages.append(message['text'])
             elif hour in evening_period:
-                evening_messages.append(message)
+                evening_messages.append(message['text'])
             elif hour in night_period:
-                night_messages.append(message)
+                night_messages.append(message['text'])
 
 # Выводим колличество сообщений для каждого времени суток
 print("Количество сообщений содержащих упоминание пива по времени суток")
@@ -54,11 +59,11 @@ print("День:", len(day_messages))
 print("Вечер:", len(evening_messages))
 print("Ночь:", len(night_messages))
 
-# Создаем диаграмму, в которой визуализируем частоту сообщений по времени суток
-labels=['Утро', 'День', 'Вечер', 'Ночь']
-values=[len(morning_messages), len(day_messages),
-            len(evening_messages), len(night_messages)]
+labels = ['Утро', 'День', 'Вечер', 'Ночь']
+values = [len(morning_messages), len(day_messages),
+          len(evening_messages), len(night_messages)]
 
+# Создаем диаграмму, в которой визуализируем частоту сообщений по времени суток
 plt.bar(labels, values)
 plt.title('Количество сообщений содержащих упоминание пива по времени суток')
 plt.xlabel('Время суток')
